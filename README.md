318f251d92b293ff550b8ad915b60088f60823d8
makgeolli <mordredphantom@gmail.com>
2023-03-23 18:03:29 -0500

    Finished password generator by adding to JavaScript file, filled in README doc with important info such as acceptance criteria, and formatted the code using Prettier

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # password-generator
+
 Consists of JavaScript, CSS, and HTML to make an online password generator
+
+The parameters that I am trying to meet with this generator is:
+
+When I click the button to generate a password, I am presented with a series of prompts for password criteria
+When prompted for password criteria, I select which criteria to include in the password
+When prompted for the length of the password, I choose a length of at least 8 characters and no more than 128 characters
+When asked for character types to include in the password, I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+When I answer each prompt, my input should be validated and at least one character type should be selected
+When all prompts are answered, a password is generated that matches the selected criteria
+When the password is generated, the password is either displayed in an alert or written to the page
+
+This is a screenshot which shows what the page is supposed to look like when working correctly:
--- script.js
+++ script.js
@@ -7,8 +7,67 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// This is the function that creates the password
+function generatePassword() {
+  var passwordLength = prompt(
+    "How many characters do you want the password to be? (Minumum: 8, Maximum: 128)"
+  );
+
+  // These are the limits for the password's length, and gives an alert window if the length is not in between these boundaries
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a password between 8 and 128 characters in length");
+    return "";
+  }
+
+  // These variables show what the characters are for each criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberChars = "0123456789";
+  var symbolChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  // As per acceptance criteria, this creates the dialogue boxes that pop up asking if different type of characters want to be included
+  var lowercaseIncluded = confirm("Include lowercase characters?");
+  var uppercaseIncluded = confirm("Include uppercase characters?");
+  var numbersIncluded = confirm("Include numbers?");
+  var symbolsIncluded = confirm("Include symbols?");
+
+  // This checks if the user wants to use the criteria or not, if they dont select any, an alert box will pop up
+  if (
+    !lowercaseIncluded &&
+    !uppercaseIncluded &&
+    !numbersIncluded &&
+    !symbolsIncluded
+  ) {
+    alert("Please select at least one character type.");
+    return "";
+  }
+
+  var allowedChar = "";
+  if (lowercaseIncluded) {
+    allowedChar += lowercaseChars;
+  }
+  if (uppercaseIncluded) {
+    allowedChar += uppercaseChars;
+  }
+  if (numbersIncluded) {
+    allowedChar += numberChars;
+  }
+  if (symbolsIncluded) {
+    allowedChar += symbolChars;
+  }
+
+  // This loop generates the password based on the inputs and criteria previously mentioned
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    password += allowedChar.charAt(
+      Math.floor(Math.random() * allowedChar.length)
+    );
+  }
+
+  return password;
+}
